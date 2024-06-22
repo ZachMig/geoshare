6556e4e7a000aa43c3068dcb2d15794777de4665
zmigliorini <zmigliorini@gmail.com>
2024-06-22 18:23:40 -0400

    user stuff

---
+++ src/main/java/com/geoshare/backend/security/UserDetailsImpl.java
@@ -0,0 +1,38 @@
+package com.geoshare.backend.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.geoshare.backend.entity.User;
+
+public class UserDetailsImpl implements UserDetails {
+	
+	private User user;
+	
+	public UserDetailsImpl(User user) {
+		this.user = user;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
