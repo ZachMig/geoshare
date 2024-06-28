05bf9aa48ca6af7f11be8c90dbe4e49c37152030
zmigliorini <zmigliorini@gmail.com>
2024-06-27 22:17:26 -0400

    setup stateless session on filterchain so csrf diabled is ok

--- src/main/java/com/geoshare/backend/config/SecurityConfig.java
+++ src/main/java/com/geoshare/backend/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -46,6 +47,7 @@ public class SecurityConfig {
                             		.anyRequest().authenticated()
                     )
                     //.formLogin(withDefaults());
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .httpBasic(withDefaults());
             return http.build();
     }
