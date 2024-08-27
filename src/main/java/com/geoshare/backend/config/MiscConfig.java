3a94bad2c7c8071737f8f5da7b89fc5cf52784f0
zmigliorini <zmigliorini@gmail.com>
2024-08-26 21:51:42 -0400

    still working on regex for maps urls, many different types apparently

--- src/main/java/com/geoshare/backend/config/MiscConfig.java
+++ src/main/java/com/geoshare/backend/config/MiscConfig.java
@@ -12,8 +12,11 @@ public class MiscConfig {
 	Pattern mapsUrlPattern() {
 		//Check if the input URL is a valid Google Maps
 		//Unsure at this time how to determine between regular Maps and Street View
+		
+		String shortURL = "^(https?:\\/\\/)?(www\\.)?maps\\.app\\.goo\\.gl\\/\\w+$";
+		String longURL = "^(https?:\\/\\/)?(www\\.)?google\\.com\\/maps\\/@.*$";
 		Pattern gMapsPattern = Pattern.compile(
-				"^(https?:\\/\\/)?(www\\.)?google\\.com\\/maps\\/@.*$",
+				shortURL + "|" + longURL,
 				Pattern.CASE_INSENSITIVE
 				);
 		return gMapsPattern;
